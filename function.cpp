ec373941078ccec6eed6530e5190da19a1015e12
dvtngan <dvtngan@gmail.com>
2021-04-10 21:18:13 +0700

    task

--- function.cpp
+++ function.cpp
@@ -1,5 +1,6 @@
 #include"functionHeader.h"
-
+#include<iostream>
+using namespace std;
 //Chau
 void createSchoolyear() {
 
@@ -9,9 +10,48 @@ void createClass() {
 
 }
 //Ngan
-void addStudentManually(string classname) {
-
+void addStudentManually(string classname){
+	ofstream fout;
+	student st;
+	fout.open(classname + ".txt",ios::app);
+	if (!fout.is_open()) {
+		cout << "This class does not exist." << endl;
+		fout.close(); return;
+	}
+	cout << "ID: "; cin >> st.studentID;
+	cout << "First name: ";
+	cin.ignore(100, '\n');
+	getline(cin, st.firstname, '\n');
+	cout << "Last name: ";
+	getline(cin, st.lastname, '\n');
+	cout << "Date of birth: ";
+	getline(cin, st.DOB, '\n');
+	st.classname = classname;
+	cout << "Gender: (1 for male, 0 for female) ";
+	cin >> st.gender;
+	while (st.gender != 1 && st.gender != 0) {
+		cout << "Gender: (1 for male, 0 for female) ";
+		cin >> st.gender;
+	}
+	cout << "Social ID: ";
+	cin >> st.socialID;
+	fout << endl;
+	fout << st.studentID << "," << st.firstname << "," << st.lastname << "," << st.DOB << ",";
+	fout << st.classname << "," << st.gender << "," << st.socialID;
+	fout.close();
+	ofstream ofile;
+	ofile.open("student.txt", ios::app);
+	ofile << endl;
+	ofile << st.studentID << "," << st.firstname << "," << st.lastname << "," << st.DOB << ",";
+	ofile << st.classname << "," << st.gender << "," << st.socialID;
+	ofile.close();
+	ofstream olog;
+	olog.open("login.txt", ios::app);
+	olog << endl;
+	olog << "student," << st.studentID << "," << st.studentID;
+	olog.close();
 }
+
 //Khoi
 void addStudentCSV(string link, string classname) {
 
--- functionHeader.h
+++ functionHeader.h
@@ -1,6 +1,9 @@
 #ifndef function_h
 #define function_h
 #include "struct.h"
+#include<iostream>
+#include<fstream>
+
 void createSchoolyear();
 void createClass();
 void addStudentManually(string classname);
