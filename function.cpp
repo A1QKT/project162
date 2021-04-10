6194aa59dd732a27aaf5518fdd5c930d72620df3
dvtngan <dvtngan@gmail.com>
2021-04-10 09:02:53 +0700

    task

--- function.cpp
+++ function.cpp
@@ -1,1 +1,18 @@
 #include"functionHeader.h"
+
+//Chau
+void createSchoolyear() {
+
+}
+//An
+void createClass() {
+
+}
+//Ngan
+void addStudentManually(string classname) {
+
+}
+//Khoi
+void addStudentCSV(string link, string classname) {
+
+}
--- functionHeader.h
+++ functionHeader.h
@@ -1,6 +1,6 @@
 #ifndef function_h
 #define function_h
-
+#include "struct.h"
 void createSchoolyear();
 void createClass();
 void addStudentManually(string classname);
