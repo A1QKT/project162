69294744b57f4476352a0247b4da73f53ae979e1
A1QKT <taquangkhoi1232002@gmail.com>
2021-04-12 20:49:18 +0700

    inputcsvfile

--- function.cpp
+++ function.cpp
@@ -1,5 +1,9 @@
 #include"functionHeader.h"
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<vector>
 using namespace std;
 //Chau
 void createSchoolyear() {
@@ -53,8 +57,38 @@ void addStudentManually(string classname){
 }
 
 //Khoi
-void addStudentCSV(string link, string classname) {
-
+void addStudentCSV(string link, student* &students, ifstream& fin) {
+	string temp, result;
+	vector<string> infoStudents(8);
+	fin.open(link);
+	if(fin.is_open()){
+		string temp1;
+		getline(fin, temp1);
+		int n;
+		stringstream inputN(temp1);
+		inputN >> n;
+		fin.ignore();
+		int j = 0;
+		while(getline(fin, temp)){
+			stringstream split(temp);
+			int i = 0;
+			while(getline(split, result, ',')){
+				infoStudents[i] = result;
+				i++;
+			}
+			//int No;int studentID;string firstname;string lastname;int gender;string DOB;int socialID;string classname;
+			stringstream no(infoStudents[0]), id(infoStudents[1]), gender(infoStudents[4]), socialid(infoStudents[6]);
+			no >> students[j].No;
+			id >> students[j].studentID;
+			students[j].firstname = infoStudents[2];
+			students[j].lastname = infoStudents[3];
+			gender >> students[j].gender;
+			students[j].DOB = infoStudents[5];
+			socialid >> students[j].socialID;
+			students[j].classname = infoStudents[7];
+		}
+	}
+	fin.close();
 }
 
 // An
